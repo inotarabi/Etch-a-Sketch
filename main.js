88995290c5b4ca6f1aba26ef1765b9dcaae6ed39
Arabi Sayed <arabisayed557@gmail.com>
2024-07-12 10:46:00 -0400

    added color functionailty + sound effect

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 const gridContainer = document.querySelector(".grid-container");
-let gridInput = document.getElementById("size-input");
+const gridInput = document.getElementById("size-input");
 const enterButton = document.querySelector("#size-enter");
 const resetButton = document.querySelector("#size-reset");
+const redButton = document.querySelector("#red");
+const blueButton = document.querySelector("#blue");
+const yellowButton = document.querySelector("#yellow");
+const greenButton = document.querySelector("#green");
+const orangeButton = document.querySelector("#orange");
+const brownButton = document.querySelector("#brown");
+const purpleButton = document.querySelector("#purple");
+const whiteButton = document.querySelector("#white");
 
+let clickSound = new Audio("sounds/click.mp3");
 let isMouseDown;
+let currentColor;
 
 enterButton.addEventListener("click", () => {
     let inputValue = gridInput.value;
@@ -14,6 +24,46 @@ resetButton.addEventListener("click", () => {
     gridContainer.textContent = "";
 });
 
+redButton.addEventListener("click", () => {
+    clickSound.play();
+    currentColor = "red";
+});
+
+blueButton.addEventListener("click", () => {
+    clickSound.play();
+    currentColor = "blue";
+});
+
+yellowButton.addEventListener("click", () => {
+    clickSound.play();
+    currentColor = "yellow";
+});
+
+greenButton.addEventListener("click", () => {
+    clickSound.play();
+    currentColor = "green";
+});
+
+orangeButton.addEventListener("click", () => {
+    clickSound.play();
+    currentColor = "orange";
+});
+
+brownButton.addEventListener("click", () => {
+    clickSound.play();
+    currentColor = "rgb(97, 18, 18)";
+});
+
+purpleButton.addEventListener("click", () => {
+    clickSound.play();
+    currentColor = "purple";
+});
+
+whiteButton.addEventListener("click", () => {
+    clickSound.play();
+    currentColor = "white";
+});
+
 function createGrid(num) {
     numBoxes = num * num;
     for (i = 1; i < numBoxes + 1; i++) {
@@ -22,15 +72,16 @@ function createGrid(num) {
         gridContainer.appendChild(gridCellDiv);
 
         cellsize = 594 / num;
+
         gridCellDiv.style.width = cellsize + "px";
 
-        color(gridCellDiv);
+        color(gridCellDiv, currentColor);
     }
 }
 
 function color(element) {
     element.addEventListener("mousedown", () => {
-        element.style.backgroundColor = "red";
+        element.style.backgroundColor = currentColor;
         isMouseDown = true;
     });
 
@@ -40,7 +91,7 @@ function color(element) {
 
     element.addEventListener("mouseover", () => {
         if (isMouseDown == true) {
-            element.style.backgroundColor = "red";
+            element.style.backgroundColor = currentColor;
         }
     });
 }
