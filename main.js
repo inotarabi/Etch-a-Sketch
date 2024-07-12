7e6e4b44054291d0b903fb3f093c3f3fdd02dce8
Arabi Sayed <arabisayed557@gmail.com>
2024-07-11 21:32:03 -0400

    dynamicaly update grid based on user input

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
 const gridContainer = document.querySelector(".grid-container");
+let gridInput = document.getElementById("size-input");
+const enterButton = document.querySelector("#size-enter");
+const resetButton = document.querySelector("#size-reset");
 
-function createGrid(num) {}
+enterButton.addEventListener("click", () => {
+    let inputValue = gridInput.value;
+    createGrid(inputValue);
+});
 
-for (i = 1; i < 257; i++) {
-    const gridCellDiv = document.createElement("div");
-    gridCellDiv.classList.add("grid-cell");
-    gridContainer.appendChild(gridCellDiv);
-    gridCellDiv.textContent = i;
+resetButton.addEventListener("click", () => {
+    gridContainer.textContent = "";
+});
+
+function createGrid(num) {
+    numBoxes = num * num;
+    for (i = 1; i < numBoxes + 1; i++) {
+        var gridCellDiv = document.createElement("div");
+        gridCellDiv.classList.add("grid-cell");
+        gridContainer.appendChild(gridCellDiv);
+
+        cellsize = 594 / num;
+        gridCellDiv.style.width = cellsize + "px";
+    }
 }
